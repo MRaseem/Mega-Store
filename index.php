dd43c614ce357fa7a9aa8c93f14a7b991457e642
MRaseem <mraseem285@gmail.com>
2023-11-06 00:23:02 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="login1.css">
+<title>Mega Store </title>
+<link rel="shortcut icon" type="image" href="./img/logo.png">
+<body>
+<div class="all-content">
+  <center>
+	  <h1 style="color:white"><b>Mega Store Sales Management System</h1>
+  </center>	
+  <div class="hero-image">
+    <div class="hero-text">
+      <img src="./img/logo.png" width="100px" alt="">
+      <h1 style="font-size:60px;color:white"><b>Welcome</h1>
+      
+        <a href="admin/dlogin.php" class="btn">Admin Login</a>
+        <a href="slogin.php"class="btn2">Store Login</a>
+        <a href="dist/dlogin.php"class="btn3">Distributor Login</a>
+      
+    </div>
+  </div>
+</div>
+</body>
+</html>
